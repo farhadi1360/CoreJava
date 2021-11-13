0a16ef6234796ca9c7bb2c9024e4bdb173f0b882
farhadi1360 <farhadi.kam@gmail.com>
2021-11-13 09:55:33 +0330

    sample for use clone method in an object

---
+++ src/main/java/ir/edu/farhadi/java/j3/Library.java
@@ -0,0 +1,24 @@
+package ir.edu.farhadi.java.j3;
+
+/**
+ * these are a lot of samples for learning core java
+ *
+ * @author Mostafa.Farhadi c@2021
+ */
+public class Test {
+    private int code;
+    private Book book;
+
+    public Test(int code, Book book) {
+        this.code = code;
+        this.book = book;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+}
