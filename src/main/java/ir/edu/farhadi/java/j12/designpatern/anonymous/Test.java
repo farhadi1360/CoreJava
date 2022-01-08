e02bdf6aff29e60b2e16bf4c59a24f6813344b66
farhadi1360 <farhadi.kam@gmail.com>
2022-01-08 17:57:19 +0330

    samples for anonymous class

--- src/main/java/ir/edu/farhadi/java/j12/designpatern/anonymous/Test.java
+++ src/main/java/ir/edu/farhadi/java/j12/designpatern/anonymous/Test.java
@@ -11,15 +11,25 @@ public class Test {
         Human student = new Human() {
             @Override
             public void eat() {
+               /* {} we can use init block
+                  instead of constructor
+                */
+                {
+                    System.out.println("washing hands");
+                }
                 System.out.println("student is eating now");
             }
         };
-
         Human personnel = new Human() {
             @Override
             public void eat() {
                 System.out.println("personnel is eating now");
             }
         };
+
+
+        student.eat();
+
+        personnel.eat();
     }
 }
